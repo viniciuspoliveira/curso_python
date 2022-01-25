eaa9c7d73d1333ca8902d7806759b837cb45e9d0
viniciuspoliveira <viniciusp.oliveira@icloud.com>
2022-01-25 05:43:36 -0300

    Mais 2 aulas de python

---
+++ aula38/aula38.py
@@ -0,0 +1,45 @@
+print()
+print('Texto explicativo')
+
+perguntas = {
+    'Pergunta 1': {
+        'pergunta': 'Quanto é 2+2?',
+        'respostas': {
+            'a': '1',
+            'b': '4',
+            'c': '5',
+            'd': '6',
+        },
+        'resposta_certa': 'b'
+    },
+    'Pergunta 2': {
+        'pergunta': 'Quanto é 3*2?',
+        'respostas': {
+            'a': '4',
+            'b': '10',
+            'c': '6',
+            'd': '8',
+        },
+        'resposta_certa': 'c'
+    },
+}
+respostas_certas = 0
+for pk, pv in perguntas.items():
+    print(f'{pk}: {pv["pergunta"]}')
+
+    print('Respostas: ')
+    for rk, rv in pv['respostas'].items():
+        print(f'[{rk}]: {rv}')
+
+    resposta_usuario = input('Sua resposta: ')
+    if resposta_usuario == pv['resposta_certa']:
+        print('EHHHHH! Voce acertou')
+        respostas_certas +=1
+    else:
+        print('Imbecil, voce errou')
+    print()
+qtd_perguntas = len(perguntas)
+porcentagem_acerto = respostas_certas / qtd_perguntas * 100
+
+print(f'Voce acertou {respostas_certas} resposta(s).')
+print(f'Sua porcentagem de acerto foi de {porcentagem_acerto}%.')
