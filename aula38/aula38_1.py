caeffc4388896ea882c9a237a9d6db50efae8e5c
viniciuspoliveira <viniciusp.oliveira@icloud.com>
2022-01-25 21:42:41 -0300

    proxima aula

---
+++ aula38/aula38_1.py
@@ -0,0 +1,12 @@
+# s1 = {1,2,3,4,5}
+#
+# for v in s1:
+#     print (v)
+
+l1 = ['Luiz', 'Fernando', 'Vinicius']
+l2 = ['Vinicius', 'Mariana', 'Francisco']
+
+if set(l1) == set(l2):
+    print('L1 é igual a L2')
+else:
+    print('L1 é diferente de L2')
